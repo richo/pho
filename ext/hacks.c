e45dabfbf6190d80a34df0f37d85ee495f70ab83
Richo Healey <richo@psych0tik.net>
2013-08-29 12:55:23 -0700

    HOW DID THIS EVER WORK D:

--- ext/hacks.c
+++ ext/hacks.c
@@ -21,7 +21,7 @@ static const char* init_arg = "embed4";
 
 char** php_init_args(void) {
     char **args;
-    args = malloc(sizeof(char) * 2);
+    args = malloc(sizeof(char*) * 2);
     args[0] = init_arg;
     args[1] = NULL;
 
