86548b4f2a3a204f4d0d642b081255a211b2f656
Richo Healey <richo@psych0tik.net>
2013-09-09 05:40:10 +0000

    typedef makes cgo sad

--- ext/hacks.h
+++ ext/hacks.h
@@ -1,10 +1,10 @@
 #ifndef _HACKS_H
 #define _HACKS_H
 
-typedef enum {
-    php_int_t,
+enum php_types {
+    php_int_t = 1,
     php_str_t
-} php_types;
+};
 
 union intern_php_type {
     long as_long;
@@ -13,7 +13,7 @@ union intern_php_type {
 };
 
 struct php_ret_t {
-    php_types typ;
+    enum php_types typ;
     union intern_php_type data;
 };
 
