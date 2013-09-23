04dd36c8eb41a6500883a4d2718b681c5af7e60c
Richo Healey <richo@psych0tik.net>
2013-09-23 11:36:44 +1000

    Imeplement a container for php interpreter contexts

--- src/pho/runtime/runtime.go
+++ src/pho/runtime/runtime.go
@@ -12,8 +12,19 @@ type PhoRuntime struct {
     tsrm unsafe.Pointer
 }
 
+type PhoContext struct {
+    rt *PhoRuntime
+    ctx unsafe.Pointer
+}
+
 func INIT() PhoRuntime {
     log.Print("initializing php runtime")
     tsrm := unsafe.Pointer(C.init_php());
     return PhoRuntime{tsrm}
+}
+
+func (rt *PhoRuntime) NewContext() PhoContext {
+    log.Print("intializing php context")
+    // TODO
+    return PhoContext{rt, nil}
 }
